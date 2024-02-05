1d3d810b94e01474bac10b2a392b73bde5f1ab95
Kaamil <kaamil6t9@gmail.com>
2024-02-05 21:28:31 +0500

    -

--- js/script.js
+++ js/script.js
@@ -1,6 +1,12 @@
 // Locomotive Smooth Scroll
-const scroll = new LocomotiveScroll({
-    el: document.querySelector('#locomotive-scroll-wrapper'),
-    smooth: true
-});
+// const scroll = new LocomotiveScroll({
+//     el: document.querySelector('#locomotive-scroll-wrapper'),
+//     smooth: true
+// });
 
+// Swiper
+var swiper = new Swiper(".mySwiper", {
+    slidesPerView: 3.5,
+    spaceBetween: 0,
+    freeMode: true,
+});
